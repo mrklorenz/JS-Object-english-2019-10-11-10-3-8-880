73ba5d0c3b30f751e49a5ebcd4c7a8849d2d3367
balajma <mark.lorenz.balajadia@oocl.com>
2021-07-23 15:20:55 +0800

    Add main.js file for exercise answers

---
+++ main.js
@@ -0,0 +1,16 @@
+//1
+var user = {
+    name : 'John',
+    surname : 'Mike'
+  }
+  user.name = 'Peter';
+  delete user.name;
+
+//2
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+};
+    
+console.log(fruit.apple + fruit.pear + fruit.peach);
